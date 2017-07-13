fcf66b000458191308e0aa5186a4b937343b6a45
Forrest VanderBorgh <forrestvanderborgh@gmail.com>
2017-07-12 18:08:26 -0700

    Initial Commit

---
+++ ARPharmaDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARPharmaDemo
+//
+//  Created by Forrest on 7/12/17.
+//  Copyright © 2017 Forrest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
